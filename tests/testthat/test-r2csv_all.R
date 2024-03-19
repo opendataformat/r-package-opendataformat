3c9e7181a8478d3bbd23326a159f8fb0c64ee062
Tom Hartl <thartl@diw.de>
2024-03-19 14:41:44 +0100

    update testscript test-r2csv

--- tests/testthat/test-r2csv_all.R
+++ tests/testthat/test-r2csv_all.R
@@ -11,9 +11,9 @@ test_that("get_dataset_all", {
   # test column 'label_de'
   expect_equal(get_dataset_all(df)[["label_de"]], "Daten vom Personenfragebogen 2010")
   # test_column 'description'
-  expect_equal(get_dataset_all(df)[["description"]], "The data were collected as part of the SOEP-Core study using the questionnaire \"Living in Germany - Survey 2010 on the social situation - Personal questionnaire for all.\" This questionnaire is addressed to the individual persons in the household. A view of the survey instrument can be found here: https://www.diw.de/documents/dokumentenarchiv/17/diw_01.c.369781.de/soepfrabo_personen_2010.pdf")
+  expect_equal(get_dataset_all(df)[["description"]], NULL)
   # test_column 'description_en'
-  expect_equal(get_dataset_all(df)[["description_en"]], "The data were collected as part of the SOEP-Core study using the questionnaire \"Living in Germany - Survey 2010 on the social situation - Personal questionnaire for all.\" This questionnaire is addressed to the individual persons in the household. A view of the survey instrument can be found here: https://www.diw.de/documents/dokumentenarchiv/17/diw_01.c.369781.de/soepfrabo_personen_2010.pdf")
+  expect_equal(get_dataset_all(df)[["description_en"]], "The data were collected as part of the SOEP-Core study using the questionnaire \"Living in Germany - Survey 2010 on the social situation - Personal questionnaire for all. This questionnaire is addressed to the individual persons in the household. A view of the survey instrument can be found here: https://www.diw.de/documents/dokumentenarchiv/17/diw_01.c.369781.de/soepfrabo_personen_2010.pdf")
   # test_column 'description_de'
   expect_equal(get_dataset_all(df)[["description_de"]], "Die Daten wurden im Rahmen der Studie SOEP-Core mittels des Fragebogens „Leben in Deutschland – Befragung 2010 zur sozialen Lage - Personenfragebogen für alle“ erhoben. Dieser Fragebogen richtet sich an die einzelnen Personen im Haushalt. Eine Ansicht des Erhebungsinstrumentes finden Sie hier: https://www.diw.de/documents/dokumentenarchiv/17/diw_01.c.369781.de/soepfrabo_personen_2010.pdf")
   # test_column 'url'
@@ -28,7 +28,6 @@ test_that("var_header", {
     "label",
     "label_en",
     "label_de",
-    "description",
     "description_en",
     "description_de",
     "type",
@@ -47,7 +46,7 @@ test_that("var_matrix", {
   # get data
   df <- get(load("testdata/data_odf.RData"))
   # test number of columns
-  expect_equal(ncol(var_matrix(df)), 9)
+  expect_equal(ncol(var_matrix(df)), 8)
   # test number of rows
   expect_equal(nrow(var_matrix(df)), 7)
 })
@@ -106,7 +105,7 @@ test_that("var_df_label", {
   # -- get data
   df <- get(load("testdata/data_odf.RData"))
   # -- test
-  expect_equal(var_df(df)[["label"]],
+  expect_equal(var_df(df)[["label_en"]],
                c("Current Health",
                  "hours of sleep, normal workday",
                  "Pressed For Time Last 4 Weeks",
@@ -120,9 +119,9 @@ test_that("var_df_label", {
   attributes(df_no_label$bap87)$label <- NULL
   attributes(df_no_label$bap9201)$label <- ""
   # -- test
-  expect_equal(var_df(df_no_label)[["label"]],
-               c("",
-                 "",
+  expect_equal(var_df(df_no_label)[["label_en"]],
+               c("Current Health",
+                 "hours of sleep, normal workday",
                  "Pressed For Time Last 4 Weeks",
                  "Run-down, Melancholy Last 4 Weeks",
                  "Well-balanced Last 4 Weeks",
@@ -136,7 +135,7 @@ test_that("var_df_label", {
   }
   # -- test
   expect_equal(colnames(var_df(df_no_label)),
-               c("variable", "label_en", "label_de", "description",
+               c("variable", "label_en", "label_de",
                  "description_en", "description_de", "type", "url" ))
 
 })
@@ -147,21 +146,21 @@ test_that("var_header_var", {
   df <- get(load("testdata/data_odf.RData"))
   # -- test
   expect_equal(var_header_var(df$bap87),
-               c("variable", "label", "label_en", "label_de", "description",
-                 "description_en", "description_de", "type", "url" ))
+               c("variable", "label_en", "label_de",
+                 "description_en", "description_de", "type", "url", "label"  ))
   # variable has no variable name
   # -- manipulate data
   attributes(df$bap87)$name <- NULL
   # -- test
   expect_equal(var_header_var(df$bap87),
-               c("variable", "label", "label_en", "label_de", "description",
-                 "description_en", "description_de", "type", "url" ))
+               c("variable", "label_en", "label_de",
+                 "description_en", "description_de", "type", "url", "label"  ))
   # variable has no label
   # -- manipulate data
   attributes(df$bap87)$label <- NULL
   # -- test
   expect_equal(var_header_var(df$bap87),
-               c("variable", "label_en", "label_de", "description",
+               c("variable", "label_en", "label_de",
                  "description_en", "description_de", "type", "url" ))
 
 })
@@ -171,7 +170,7 @@ test_that("var_matrix_var", {
   # -- get data
   df <- get(load("testdata/data_odf.RData"))
   # -- test
-  expect_equal(ncol(var_matrix_var(df$bap87)), 9)
+  expect_equal(ncol(var_matrix_var(df$bap87)), 8)
   expect_equal(nrow(var_matrix_var(df$bap87)), 1)
   # variable has no name, label and url
   # -- manipulate data
@@ -179,7 +178,7 @@ test_that("var_matrix_var", {
   attributes(df$bap87)$label <- NULL
   attributes(df$bap87)$url <- NULL
   # -- test
-  expect_equal(ncol(var_matrix_var(df$bap87)), 7)
+  expect_equal(ncol(var_matrix_var(df$bap87)), 6)
   expect_equal(nrow(var_matrix_var(df$bap87)), 1)
 })
 #' var_variable_column_var: variable input - get metadata for 'variable' column
@@ -246,8 +245,7 @@ test_that("cat_header_data", {
   # -- get data
   df <- get(load("testdata/data_odf.RData"))
   # -- test
-  expect_equal(cat_header_data(df), c("variable", "value", "labels",
-  "labels_en", "labels_de"))
+  expect_equal(cat_header_data(df), c("variable", "value", "labels_en", "labels_de"))
 })
 #' cat_variable_column_data: data set input - get column 'variable'
 #' for categories.csv
@@ -279,11 +277,11 @@ test_that("cat_variable_column_data", {
   # -- manipulate data: empty labels
   df_empty_labels <- df
   attributes(df_empty_labels$bap9001)$labels <- ""
-  expect_equal(length(cat_variable_column_data(df_empty_labels)), 28)
+  expect_equal(length(cat_variable_column_data(df_empty_labels)), 34)
   # -- manipulate data: no labels
   df_no_labels <- df
   attributes(df_no_labels$bap9001)$labels <- NULL
-  expect_equal(length(cat_variable_column_data(df_no_labels)), 27)
+  expect_equal(length(cat_variable_column_data(df_no_labels)), 34)
 })
 #' cat_matrix_data: data set input - matrix for categories.csv
 test_that("cat_matrix_data", {
@@ -291,17 +289,19 @@ test_that("cat_matrix_data", {
   df <- get(load("testdata/data_odf.RData"))
   # --- test
   expect_equal(nrow(cat_matrix_data(df)), 34) # nrow
-  expect_equal(ncol(cat_matrix_data(df)), 5) # ncol
+  expect_equal(ncol(cat_matrix_data(df)), 4) # ncol
   # -- manipulate data: one variable is not categorical
-  attributes(df$bap96)$labels <- NULL
+  attributes(df$bap96)$labels_de <- NULL
+  attributes(df$bap96)$labels_en <- NULL
   # --- test
   expect_equal(nrow(cat_matrix_data(df)), 32) # nrow
-  expect_equal(ncol(cat_matrix_data(df)), 5) # ncol
+  expect_equal(ncol(cat_matrix_data(df)), 4) # ncol
   # -- manipulate data: another variable is categorical but labels are empty
-  attributes(df$bap87)$labels <- ""
+  attributes(df$bap87)$labels_en <- ""
+  attributes(df$bap87)$labels_de <- ""
   # --- test
   expect_equal(nrow(cat_matrix_data(df)), 26) # nrow
-  expect_equal(ncol(cat_matrix_data(df)), 5) # ncol
+  expect_equal(ncol(cat_matrix_data(df)), 4) # ncol
 })
 #' cat_values_column_data: data set input - get column 'values'
 #' for categories.csv
@@ -315,11 +315,13 @@ test_that("cat_values_column_data", {
     "2",  "3",  "4",  "5",  "-2", "-1", "-2", "-1")
     )
   # -- manipulate data: one variable is not categorical
-  attributes(df$bap96)$labels <- NULL
+  attributes(df$bap96)$labels_de <- NULL
+  attributes(df$bap96)$labels_en <- NULL
   # --- test
   expect_equal(length(cat_values_column_data(df)), 32)
   # -- manipulate data: another variable is categorical but labels are empty
-  attributes(df$bap87)$labels <- ""
+  attributes(df$bap87)$labels_de <- ""
+  attributes(df$bap87)$labels_en <- ""
   # --- test
   expect_equal(length(cat_values_column_data(df)), 26)
 })
@@ -329,7 +331,7 @@ test_that("cat_labels_column_data", {
   # -- get data
   df <- get(load("testdata/data_odf.RData"))
   # --- test
-  expect_equal(cat_labels_column_data(df, "labels"), c(
+  expect_equal(cat_labels_column_data(df, "labels_en"), c(
     "Does not apply", "No Answer", "Very good", "Good", "Satisfactory", "Poor",
     "Bad", "Does not apply", "No Answer", "Does not apply", "No Answer",
     "Always", "Often", "Sometimes", "Almost Never", "Never",  "Does not apply",
@@ -338,13 +340,15 @@ test_that("cat_labels_column_data", {
     "Never", "Does not apply", "No Answer", "Does not apply", "No Answer")
     )
   # -- manipulate data: one variable is not categorical
-  attributes(df$bap96)$labels <- NULL
+  attributes(df$bap96)$labels_de <- NULL
+  attributes(df$bap96)$labels_en <- NULL
   # --- test
-  expect_equal(length(cat_labels_column_data(df, "labels")), 32)
+  expect_equal(length(cat_labels_column_data(df, "labels_de")), 32)
   # -- manipulate data: another variable is categorical but labels are empty
-  attributes(df$bap87)$labels <- ""
+  attributes(df$bap87)$labels_de <- ""
+  attributes(df$bap87)$labels_en <- ""
   # --- test
-  expect_equal(length(cat_labels_column_data(df, "labels")), 26)
+  expect_equal(length(cat_labels_column_data(df, "labels_de")), 26)
 })
 #' cat_df_data: data set input - make categories data frame
 test_that("cat_df_data",{
@@ -381,7 +385,8 @@ test_that("cat_df_data",{
     "trifft nicht zu", "keine Angabe",  "trifft nicht zu",  "keine Angabe")
   )
   # -- manipulate data: empty labels and empty variable name
-  attributes(df$bap87)$labels <- ""
+  attributes(df$bap87)$labels_en <- ""
+  attributes(df$bap87)$labels_de <- ""
   attributes(df$bap87)$name <- ""
   # --- test
   expect_equal(cat_df_data(df)[["labels_de"]], c(
@@ -424,7 +429,8 @@ test_that("cat_df_data",{
   )
   # -- manipulate data: no variable name and no labels
   attributes(df$bap87)$name <- NULL
-  attributes(df$bap87)$labels <- NULL
+  attributes(df$bap87)$labels_de <- NULL
+  attributes(df$bap87)$labels_en <- NULL
   expect_equal(cat_df_data(df)[["variable"]], c(
     "bap9201", "bap9201",
     "bap9001", "bap9001", "bap9001", "bap9001", "bap9001", "bap9001", "bap9001",
@@ -448,32 +454,32 @@ test_that("cat_header_var", {
   df <- get(load("testdata/data_odf.RData"))
   # --- test
   expect_equal(cat_header_var(df$bap87), c(
-    "variable", "value", "labels", "labels_en", "labels_de")
+    "variable", "value", "labels_en", "labels_de")
   )
   # --- manipulate data: empty variable names
   attributes(df$bap87)$name <- ""
   # --- test
   expect_equal(cat_header_var(df$bap87), c(
-    "variable", "value", "labels", "labels_en", "labels_de")
+    "variable", "value", "labels_en", "labels_de")
   )
   # --- manipulate data: no variable names
   attributes(df$bap87)$name <- NULL
   # --- test
   expect_equal(cat_header_var(df$bap87), c(
-    "variable", "value", "labels", "labels_en", "labels_de")
+    "variable", "value", "labels_en", "labels_de")
   )
   # --- manipulate data: empty variable labels
-  attributes(df$bap87)$labels <- ""
-  # --- test
-  expect_equal(cat_header_var(df$bap87), c(
-    "variable", "value", "labels", "labels_en", "labels_de")
-  )
-  # --- manipulate data: no variable labels
-  attributes(df$bap87)$labels <- NULL
-  print(attributes(df$bap87)$labels)
+  attributes(df$bap87)$labels_en <- ""
   # --- test
   expect_equal(cat_header_var(df$bap87), c(
     "variable", "value", "labels_en", "labels_de")
+  )
+  # --- manipulate data: no variable labels
+  attributes(df$bap87)$labels_de <- NULL
+  print(attributes(df$bap87)$labels_de)
+  # --- test
+  expect_equal(cat_header_var(df$bap87), c(
+    "variable", "value", "labels_en")
   )
 })
 #' cat_variable_column_var: variable input - get column 'variable'
@@ -504,14 +510,16 @@ test_that("cat_matrix_var", {
   df <- get(load("testdata/data_odf.RData"))
   # --- test
   expect_equal(nrow(cat_matrix_var(df$bap96)), 2)
-  expect_equal(ncol(cat_matrix_var(df$bap96)), 5)
+  expect_equal(ncol(cat_matrix_var(df$bap96)), 4)
   # -- manipulate data: empty labels
-  attributes(df$bap96)$labels <- ""
+  attributes(df$bap96)$labels_de <- ""
+  attributes(df$bap96)$labels_en <- ""
   # --- test
   expect_equal(nrow(cat_matrix_var(df$bap96)), 1)
-  expect_equal(ncol(cat_matrix_var(df$bap96)), 5)
+  expect_equal(ncol(cat_matrix_var(df$bap96)), 4)
   # -- manipulate data: no labels
-  attributes(df$bap96)$labels <- NULL
+  attributes(df$bap96)$labels_de <- NULL
+  attributes(df$bap96)$labels_en <- NULL
   # --- test
   expect_equal(nrow(cat_matrix_var(df$bap96)), NULL)
   expect_equal(ncol(cat_matrix_var(df$bap96)), NULL)
@@ -524,11 +532,13 @@ test_that("cat_values_column_var", {
   expect_equal(cat_values_column_var(df$bap87), c(
     "-2", "-1", "1",  "2",  "3",  "4",  "5"))
   # -- manipulate data: empty labels
-  attributes(df$bap87)$labels <- ""
+  attributes(df$bap87)$labels_de <- ""
+  attributes(df$bap87)$labels_en <- ""
   # --- test
   expect_equal(cat_values_column_var(df$bap87), "")
   # -- manipulate data: no labels
-  attributes(df$bap87)$labels <- NULL
+  attributes(df$bap87)$labels_de <- NULL
+  attributes(df$bap87)$labels_en <- NULL
   # --- test
   expect_equal(cat_values_column_var(df$bap87), NULL)
 })
@@ -538,7 +548,7 @@ test_that("cat_labels_column_var'", {
     # -- get data
     df <- get(load("testdata/data_odf.RData"))
     # --- test
-    expect_equal(cat_labels_column_var(df$bap87, "labels"), c(
+    expect_equal(cat_labels_column_var(df$bap87, "labels_en"), c(
       "Does not apply", "No Answer",  "Very good",  "Good", "Satisfactory",
       "Poor", "Bad")
     )
@@ -547,13 +557,14 @@ test_that("cat_labels_column_var'", {
       "Zufriedenstellend", "Weniger gut",  "Schlecht" )
     )
     # -- manipulate data: empty labels
-    attributes(df$bap87)$labels <- ""
+    attributes(df$bap87)$labels_de <- ""
+    attributes(df$bap87)$labels_en <- ""
     # --- test
-    expect_equal(cat_labels_column_var(df$bap87, "labels"), "")
+    expect_equal(cat_labels_column_var(df$bap87, "labels_de"), "")
     # -- manipulate data: no labels
-    attributes(df$bap87)$labels <- NULL
+    attributes(df$bap87)$labels_en <- NULL
     # --- test
-    expect_equal(cat_labels_column_var(df$bap87, "labels"), NULL)
+    expect_equal(cat_labels_column_var(df$bap87, "labels_en"), NULL)
 
 })
 #' cat_df_var: variable input - get categories data frame
@@ -576,8 +587,8 @@ test_that("cat_df_var", {
   expect_equal(cat_df_var(df$bap87)[["variable"]], rep("var_id", 7)
   )
   # manipulate data: empty label
-  attributes(df$bap96)$labels <- ""
-  expect_equal(cat_df_var(df$bap96)[["labels"]], "")
+  attributes(df$bap96)$labels_de <- ""
+  expect_equal(cat_df_var(df$bap96)[["labels_de"]], c("",""))
   # manipulate data: no label
   attributes(df$bap96)$labels <- NULL
   expect_equal(cat_df_var(df$bap96)[["labels"]], NULL)
